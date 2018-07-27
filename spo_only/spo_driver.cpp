fdc7faddacfa26f1301711301fbce4a9395c2233
Mark Dewing <markdewing@gmail.com>
2018-07-27 11:05:00 -0500

    Add RealType to choose float or double.

--- spo_only/spo_driver.cpp
+++ spo_only/spo_driver.cpp
@@ -20,8 +20,10 @@ inline double cpu_clock()
   return (double)tv.tv_sec+(1.e-6)*tv.tv_usec;
 }
 
+//typedef double RealType;
+typedef float RealType;
 
-void setup_spline_data(bspline_traits<double, 3>::SplineType &spline_data, int nspline)
+void setup_spline_data(bspline_traits<RealType, 3>::SplineType &spline_data, int nspline)
 {
   int grid_num = 37;
   int coef_num = grid_num + 3;
@@ -53,14 +55,14 @@ void setup_spline_data(bspline_traits<double, 3>::SplineType &spline_data, int n
   spline_data.z_stride = nspline;
 
   int coef_size = coef_num * coef_num * coef_num * nspline;
-  spline_data.coefs = (double *)malloc(sizeof(double) * coef_size);
+  spline_data.coefs = (RealType *)malloc(sizeof(RealType) * coef_size);
 
-  size_t coef_bytes = coef_size * sizeof(double);
+  size_t coef_bytes = coef_size * sizeof(RealType);
   double coef_mb = coef_bytes/(1024.0)/1024; // in MB
   std::cout << " Coefficient size (MB) = " << coef_mb << std::endl;
 
   std::mt19937 e1(1);
-  std::uniform_real_distribution<double> uniform(0.0, 1.0);
+  std::uniform_real_distribution<RealType> uniform(0.0, 1.0);
   for (int i = 0; i < coef_size; i++) {
     spline_data.coefs[i] = uniform(e1);
   }
@@ -88,28 +90,28 @@ int main(int argc, char **argv)
   int ntiles = 1;
 
 
-  std::vector<double> psi, psi_ref;
+  std::vector<RealType> psi, psi_ref;
   psi.resize(nspline);
   psi_ref.resize(nspline);
 
-  std::vector<double> grad, grad_ref;
+  std::vector<RealType> grad, grad_ref;
   grad.resize(3*nspline);
   grad_ref.resize(3*nspline);
 
-  std::vector<double> lapl, lapl_ref;
+  std::vector<RealType> lapl, lapl_ref;
   lapl.resize(3*nspline);
   lapl_ref.resize(3*nspline);
 
-  double pos[3] = {0.0, 0.0, 0.0};
-  bspline_traits<double, 3>::SplineType spline_data;
+  RealType pos[3] = {0.0, 0.0, 0.0};
+  bspline_traits<RealType, 3>::SplineType spline_data;
 
   setup_spline_data(spline_data, nspline);
 
-  miniqmcreference::MultiBsplineRef<double> spline_ref;
+  miniqmcreference::MultiBsplineRef<RealType> spline_ref;
 
   int nrepeat = 100;
 
-  int coef_bytes_loaded = nspline * 64 * sizeof(double);
+  int coef_bytes_loaded = nspline * 64 * sizeof(RealType);
   // Ops numbers from hand-counting.
   int vgl_adds = 3 + 12*3*3 + ((3*3 + 7)*nspline + 6)*16 + nspline*2;
   int vgl_muls = 12*3*3 + ((4*3 + 7)*nspline)*16 + 3 + nspline*6;
@@ -135,7 +137,7 @@ int main(int argc, char **argv)
   std::cout << std::endl;
 
 
-  MultiBspline<double> spline_new;
+  MultiBspline<RealType> spline_new;
   double start = cpu_clock();
   for (int i = 0; i < nrepeat; i++) {
     //spline_new.evaluate_v(&spline_data, pos[0], pos[1], pos[2], psi.data(), nspline);
